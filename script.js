f0845119d13e06181327472685662901d67ca0d2
Melisa Lachev <melisatormac55@gmail.com>
2025-06-27 00:13:12 +0300

    used functionality scrollIntoView in navbar event handler

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ const header = document.querySelector(`.header-section`);
 navBtn.addEventListener(`click`, function () {
   header.classList.toggle(`nav--open`);
 });
+
+const howBtn = document.querySelector(`#how`)
+const howSection = document.querySelector(`.section-how`)
+const navBar = document.querySelector(`.nav-bar`)
+const mealBtn = document.querySelector(`#meals`)
+const mealsSection = document.querySelector(`.meals-section`)
+const testimonialsBtn = document.querySelector(`#testimonials`)
+const testimonialSection = document.querySelector(`.testimonials-section`)
+const pricingBtn = document.querySelector(`#pricing`)
+const pricingSection = document.querySelector(`.section-pricing`)
+const ctaBtn = document.querySelector(`#cta`)
+const ctaSection = document.querySelector(`.cta-section`)
+
+navBar.addEventListener(`click`, function (e) {
+    if (!e.target.classList.contains(`section-nav-link`)) return
+    if(e.target === howBtn) howSection.scrollIntoView({ behavior: 'smooth' })
+    if(e.target === mealBtn) mealsSection.scrollIntoView({ behavior: 'smooth' })
+    if(e.target === testimonialsBtn) testimonialSection.scrollIntoView({ behavior: 'smooth' })
+    if(e.target === pricingBtn) pricingSection.scrollIntoView({ behavior: 'smooth' })
+    if(e.target === ctaBtn) ctaSection.scrollIntoView({ behavior: 'smooth' })
+})
